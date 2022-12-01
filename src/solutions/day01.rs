12f15ac75c778540c0bf953e0b3421a8d078c44f
RayhaanT <tanweer.rayhaan@gmail.com>
2022-12-01 00:30:28 -0500

    Done day 1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,8 +1,22 @@
 use crate::Solution;
 
-pub fn solve(_input: String) -> Solution {
+pub fn solve(input: String) -> Solution {
+    let mut inventories: Vec<i32> = input
+        .trim_end()
+        .split("\n\n")
+        .map(|inv| {
+            inv.split("\n")
+                .fold(0, |acc, item| acc + item.parse::<i32>().unwrap())
+        })
+        .collect();
+
+    inventories.sort_unstable_by(|a, b| b.cmp(a));
+
     Solution {
-        first: String::from("Incomplete"),
-        second: String::from("Incomplete"),
+        first: inventories[0].to_string(),
+        second: inventories[0..3]
+            .iter()
+            .fold(0, |acc, item| acc + item)
+            .to_string(),
     }
 }
