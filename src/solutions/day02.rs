11cd747a36468124a6f82c8ccdc8384ba6191690
RayhaanT <tanweer.rayhaan@gmail.com>
2022-12-01 00:47:35 -0500

    Cleaned up day 2 soln

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -3,7 +3,7 @@ use crate::Solution;
 fn result(a: u64, b: u64) -> u64 {
     if a == b {
         1
-    } else if a + 1 == b || a + 1 == 4 && b == 1 {
+    } else if (a + 1) % 3 == b {
         2
     } else {
         0
@@ -11,37 +11,32 @@ fn result(a: u64, b: u64) -> u64 {
 }
 
 pub fn solve(input: String) -> Solution {
-    let pairs: Vec<(u64, u64)> = input
-        .trim_end()
-        .split("\n")
-        .map(|l| {
-            (
-                match l.chars().nth(0).unwrap() {
-                    'A' => 1,
-                    'B' => 2,
-                    'C' => 3,
-                    _ => panic!("Bad char"),
-                },
-                match l.chars().nth(2).unwrap() {
-                    'X' => 1,
-                    'Y' => 2,
-                    'Z' => 3,
-                    _ => panic!("Bad char"),
-                },
-            )
-        })
-        .collect();
+    let pairs = input.trim_end().split("\n").map(|l| {
+        (
+            match l.chars().nth(0).unwrap() {
+                'A' => 0,
+                'B' => 1,
+                'C' => 2,
+                _ => panic!("Bad char"),
+            },
+            match l.chars().nth(2).unwrap() {
+                'X' => 0,
+                'Y' => 1,
+                'Z' => 2,
+                _ => panic!("Bad char"),
+            },
+        )
+    });
 
     let sum: u64 = pairs
         .clone()
-        .iter()
-        .fold(0, |acc, pair| acc + pair.1 + 3 * result(pair.0, pair.1));
+        .fold(0, |acc, pair| acc + 1 + pair.1 + 3 * result(pair.0, pair.1));
 
-    let sum2 = pairs.iter().fold(0, |acc, pair| {
+    let sum2 = pairs.fold(0, |acc, pair| {
         acc + (match pair.1 {
-            1 => 0 + (if pair.0 == 1 { 3 } else { pair.0 - 1 }),
-            2 => 3 + pair.0,
-            3 => 6 + (if pair.0 == 3 { 1 } else { pair.0 + 1 }),
+            0 => 0 + 1 + ((pair.0 + 2) % 3),
+            1 => 3 + 1 + pair.0,
+            2 => 6 + 1 + ((pair.0 + 1) % 3),
             _ => panic!("Should not be possible"),
         })
     });
