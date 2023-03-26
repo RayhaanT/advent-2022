b684ae17dccbbe96021d5faf5a5ecb3e1bd2a9b0
RayhaanT <tanweer.rayhaan@gmail.com>
2023-03-26 02:12:54 -0400

    More efficient day 12

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,17 +1,30 @@
 use crate::Solution;
-use std::collections::HashSet;
 use std::collections::HashMap;
 
-fn shortest_path(start: (usize, usize), grid: &Vec<Vec<char>>) -> u32 {
+pub fn solve(input: String) -> Solution {
+    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let mut start: (usize, usize) = (0, 0);
+    let mut end: (usize, usize) = (0, 0);
+    for (y, row) in grid.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            if *cell == 'S' {
+                start = (y, x);
+            }
+            if *cell == 'E' {
+                end = (y, x);
+            }
+        }
+    }
+
     let height = grid.len();
     let width = grid[0].len();
+    let mut best_dists: Vec<Vec<u32>> = vec![vec![u32::MAX; width]; height];
+    best_dists[end.0][end.1] = 0;
 
     let mut open: HashMap<(usize, usize), u32> = HashMap::new();
-    let mut closed: HashSet<(usize, usize)> = HashSet::new();
-    open.insert(start, 0);
+    open.insert(end, 0);
 
     let mut current = start;
-    let mut res = u32::MAX;
 
     while !open.is_empty() {
         let mut cost = u32::MAX;
@@ -21,33 +34,51 @@ fn shortest_path(start: (usize, usize), grid: &Vec<Vec<char>>) -> u32 {
                 current = *o.0;
             }
         }
-
-        if grid[current.0][current.1] == 'E' {
-            res = cost;
-            break;
-        }
+        best_dists[current.0][current.1] = cost;
 
         for dir in 0..4 {
             let neighbour = match dir {
                 0 => (current.0 + 1, current.1),
-                1 => if current.0 > 0 { (current.0 - 1, current.1) } else { current },
+                1 => {
+                    if current.0 > 0 {
+                        (current.0 - 1, current.1)
+                    } else {
+                        current
+                    }
+                }
                 2 => (current.0, current.1 + 1),
-                3 => if current.1 > 0 { (current.0, current.1 - 1) } else { current },
-                _ => panic!("Bad direction")
+                3 => {
+                    if current.1 > 0 {
+                        (current.0, current.1 - 1)
+                    } else {
+                        current
+                    }
+                }
+                _ => panic!("Bad direction"),
             };
 
             if neighbour == current {
                 continue;
             }
 
-            let curr_elevation = if grid[current.0][current.1] == 'S' { 'a' as u32 } else { grid[current.0][current.1] as u32 };
+            let curr_elevation = if grid[current.0][current.1] == 'E' {
+                'z' as u32
+            } else {
+                grid[current.0][current.1] as u32
+            };
 
-            if neighbour.0 < height && neighbour.1 < width  {
-                if closed.contains(&neighbour) {
+            if neighbour.0 < height && neighbour.1 < width {
+                if best_dists[neighbour.0][neighbour.1] != u32::MAX {
                     continue;
                 }
-                let elevation = if grid[neighbour.0][neighbour.1] == 'E' { 'z' as u32 } else { grid[neighbour.0][neighbour.1] as u32 };
-                if curr_elevation + 1 < elevation {
+
+                let elevation = if grid[neighbour.0][neighbour.1] == 'S' {
+                    'a' as u32
+                } else {
+                    grid[neighbour.0][neighbour.1] as u32
+                };
+
+                if curr_elevation > elevation + 1 {
                     continue;
                 }
                 open.entry(neighbour).or_insert(cost + 1);
@@ -55,38 +86,24 @@ fn shortest_path(start: (usize, usize), grid: &Vec<Vec<char>>) -> u32 {
         }
 
         open.remove(&current);
-        closed.insert(current);
     }
 
-    res
-}
-
-pub fn solve(input: String) -> Solution {
-    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
-    let mut start: (usize, usize) = (0, 0);
-    for (y, row) in grid.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            if *cell == 'S' {
-                start = (y, x);
-            }
-        }
-    }
-
-    let start_cost = shortest_path(start, &grid);
-
-    let mut best_cost = start_cost;
-    for (y, row) in grid.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            if *cell == 'a' {
-                // let res = shortest_path((y, x), &grid);
-                // println!("({} {}) = {}", x, y, res);
-                best_cost = best_cost.min(shortest_path((y, x), &grid))
-            }
-        }
-    }
+    let best_cost = best_dists
+        .iter()
+        .enumerate()
+        .fold(u32::MAX, |res, (y, next)| {
+            let row_best = next.iter().enumerate().fold(u32::MAX, |res, (x, next)| {
+                if grid[y][x] == 'a' {
+                    res.min(*next)
+                } else {
+                    res
+                }
+            });
+            res.min(row_best)
+        });
 
     Solution {
-        first: start_cost.to_string(),
+        first: best_dists[start.0][start.1].to_string(),
         second: best_cost.to_string(),
     }
 }
